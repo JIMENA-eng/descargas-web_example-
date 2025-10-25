0cde3ed0bcd4200cab2bcd3e134babb331d25ae0
JIMENA-eng <bokatsudeku@gmail.com>
2025-10-25 16:32:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
         url = request.form["url"]
 
         # Llamada a la API simple-ytdl
-        api_url = f"https://simple-ytdl.vercel.app/api/video?url={url}"
+        api_url = f"https://api.youtubedownloader.io/api/v1/info?url={url}"
         response = requests.get(api_url)
 
         if response.status_code != 200:
@@ -24,4 +24,5 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
